4998423df446e314483132d5b601b1e8fab5029b
Furkan Alioglu <91939823+furkanalioglu@users.noreply.github.com>
2023-10-13 22:28:53 +0300

    xcode files

---
+++ CleanSwiftUI 22.27.54/CleanSwiftUI/CleanSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanSwiftUIApp.swift
+//  CleanSwiftUI
+//
+//  Created by Furkan Alioglu on 13.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct CleanSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
